fe1b7fb201cc4a57b9a1795655323b5e559cf5cd
Will Jackson <wrejcodes@gmail.com>
2023-02-12 18:10:15 -0600

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <Layout>
+
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+
+    <h1>Welcome to my gridsome blog</h1>
+
+    <p>
+      I'm a developer and I make things. Sometimes I'll talk about it here.
+    </p>
+
+    <p class="home-links">
+      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
+      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+    </p>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'wrejcodes'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
