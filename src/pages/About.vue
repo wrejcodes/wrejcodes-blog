6b4d60d3ef597e2b26548775cd2d11c401c32e26
Will Jackson <wrejcodes@gmail.com>
2023-02-12 21:49:14 -0600

    blog-5 Added tailwindcss to project  - Added autoprefixer and postcss  - Added theme colors  - Added some styles to pages and markdown content

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    <h1>About me</h1>
+    <h1 class="text-xl font-semibold mb-5">About me</h1>
     <p>I work for SpyFu. Using this blog to learn gridsome.</p>
   </Layout>
 </template>
---
+++ src/pages/Blog.vue
@@ -0,0 +1,33 @@
+<template>
+    <Layout>
+        <h1 class="text-xl font-semibold mb-5">Pages</h1>
+
+        <ul class="list-outside list-disc">
+            <li v-for="post in $page.posts.edges" :key="post.path" class="mt-3">
+                <g-link :to="post.node.path">{{ post.node.title }} - {{ post.node.date }}</g-link>
+            </li>
+        </ul>
+    </Layout>
+</template>
+
+<page-query>
+query Posts {
+    posts: allBlogPost (sortBy: "date", order: DESC) {
+        edges {
+            node {
+                title
+                date (format: "MMMM D, Y")
+                path
+            }
+        }
+    }
+}
+</page-query>
+
+<script>
+export default {
+    metaInfo: {
+        title: 'Blog Posts'
+    }
+}
+</script>
