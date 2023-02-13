9b7610dbf4c4fab59cf8e68bca83ae383c7c2d7d
wrejcodes <wrejcodes@gmail.com>
2023-02-12 18:36:30 -0600

    Update src/pages/About.vue

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <h1>About me</h1>
-    <p>I work for SpyFu. Working on learning gridsome.</p>
+    <p>I work for SpyFu. Using this blog to learn gridsome.</p>
   </Layout>
 </template>
 
